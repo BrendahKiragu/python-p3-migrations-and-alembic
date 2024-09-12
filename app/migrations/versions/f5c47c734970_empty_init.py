67e2ab63af394b3db8a83285ad2a50283d69e3ac
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-12 21:11:43 +0300

    adds alembic to manage migrations

---
+++ app/migrations/versions/f5c47c734970_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f5c47c734970
+Revises: 
+Create Date: 2024-09-12 20:41:36.224319
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f5c47c734970'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
